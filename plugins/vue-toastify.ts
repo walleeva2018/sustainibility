9a858e62f92c51e38b5d6a6478521a625c3e66c0
zubairrafi <walleeva2018@gmail.com>
2024-02-21 15:17:28 +0600

    toastify fixed
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- plugins/vue-toastify.ts
+++ plugins/vue-toastify.ts
@@ -1,1 +1,12 @@
-export const myString = 'Hello from my plugin'
+import Vue3Toastify, { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Vue3Toastify, { autoClose: 1000, position: 'top-center' })
+
+  return {
+    provide: {
+      toast,
+    },
+  }
+})
