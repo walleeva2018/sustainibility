e9824deba09ea275ebda9a797da477da7e94adc7
unknown <mail.tahsin99@gmail.com>
2024-05-13 23:00:32 +0600

    about page & course page responsive
    Signed-off-by: unknown <mail.tahsin99@gmail.com>

--- pages/courses/[course].vue
+++ pages/courses/[course].vue
@@ -19,7 +19,8 @@ const contentTable = [{
   time: '10min',
   url: 'https://www.youtube.com/embed/R0ksvPPrQkg?si=inj3D1E2pPj-Tafe',
   completed: true,
-}]
+},
+]
 const selecedId = ref(0)
 const selectedVideoUrl = ref(contentTable[selecedId.value].url)
 const selecedVideTitle = ref(contentTable[selecedId.value].title)
@@ -35,7 +36,7 @@ function prevNextButton(id: number) {
 </script>
 
 <template>
-  <div class="sm:grid grid-cols-8 px-6 py-5 sm:py-9 gap-5 container max-w-6xl mt-16 mx-auto">
+  <div class="sm:grid grid-cols-8 px-2 py-5 sm:py-9 gap-5 container max-w-6xl mt-10 mx-auto">
     <div class="one sm:col-span-6">
       <iframe
         class="w-full h-96"
@@ -44,7 +45,7 @@ function prevNextButton(id: number) {
         frameborder="0"
         allowfullscreen
       />
-      <div class="flex justify-between mb-20">
+      <div class="flex justify-between sm:mb-20 mb-10">
         <button :disabled="selecedId === 0" @click="prevNextButton(selecedId - 1)">
           Prev
         </button>
@@ -52,13 +53,31 @@ function prevNextButton(id: number) {
           Next
         </button>
       </div>
+      <div class="sm:hidden sm:col-span-2 border border-gray-300 rounded-lg p-3 mb-3">
+        <p>Table Of Content</p>
+        <div class="h-24 overflow-y-auto">
+          <div v-for="content in contentTable" :key="content.id" @click="changeVideo(content.id)">
+            <div class="flex items-center">
+              <svg v-if="selecedId === content.id" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="fill-current text-gray-600">
+                <path d="m9.5 16.5l7-4.5l-7-4.5zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
+              </svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                <path fill="currentColor" d="m9.5 16.5l7-4.5l-7-4.5zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" />
+              </svg>
+              <div class="ml-2">
+                <p>{{ content.title }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <p class="font-bold text-5xl">
         {{ selecedVideTitle }}
       </p>
       <hr>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nemo rem iure iusto praesentium cupiditate earum cumque aspernatur asperiores nisi vitae rerum explicabo provident magnam consectetur, assumenda a dolor perspiciatis.</p>
     </div>
-    <div class="two sm:col-span-2 border border-gray-300 rounded-lg p-3 mb-3">
+    <div class="two hidden sm:block sm:col-span-2 border border-gray-300 rounded-lg p-3 mb-3">
       <p>Table Of Content</p>
       <div v-for="content in contentTable" :key="content.id" @click="changeVideo(content.id)">
         <div class="flex items-center">
